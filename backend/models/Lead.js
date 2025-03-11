9a00ddb4a7a0779d496b22b98c58fdde19bf895f
MwirigiMuriithi <learnerviner@gmail.com>
2025-03-11 22:12:02 +0300

    added express validator for validation to backend/models/Lead.js

--- backend/models/Lead.js
+++ backend/models/Lead.js
@@ -2,7 +2,7 @@ import mongoose from 'mongoose';
 
 const LeadSchema = new mongoose.Schema({
   name: { type: String, required: true },
-  email: { type: String, required: true, unique: true },
+  email: { type: String, required: true, },
   status: { 
     type: String, 
     enum: ["New", "Engaged", "Proposal Sent", "Closed-Won", "Closed-Lost"],

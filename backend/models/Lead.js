cced2908188805b48fbfd5f3d65768f8a8b3cb3d
MwirigiMuriithi <learnerviner@gmail.com>
2025-03-11 22:55:42 +0300

    updated models/Lead.js

--- backend/models/Lead.js
+++ backend/models/Lead.js
@@ -2,7 +2,7 @@ import mongoose from 'mongoose';
 
 const LeadSchema = new mongoose.Schema({
   name: { type: String, required: true },
-  email: { type: String, required: true, },
+  email: { type: String, required: true, unique: true },
   status: { 
     type: String, 
     enum: ["New", "Engaged", "Proposal Sent", "Closed-Won", "Closed-Lost"],
